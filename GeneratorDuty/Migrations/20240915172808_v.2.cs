ed8214cb021fdb011dd52261a5fe916cdbc5bc70
TheCraztWolf <tcrazywolf63@gmail.com>
2024-09-15 21:28:28 +0400

    Новые миграции

---
+++ GeneratorDuty/Migrations/20240915172808_v.2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneratorDuty.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAutoExport",
+                table: "ScheduleProps",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAutoExport",
+                table: "ScheduleProps");
+        }
+    }
+}
