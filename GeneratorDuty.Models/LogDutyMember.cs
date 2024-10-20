fc4c5c541a1569e989656db1d2731bbcc54aa117
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-20 13:35:13 +0400

    Модель истории дежурств

---
+++ GeneratorDuty.Models/LogDutyMember.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using GeneratorDuty.Models.Common;
+
+namespace GeneratorDuty.Models;
+
+public class LogDutyMember : CommonEntity
+{
+    public long? UserId { get; set; }
+    [ForeignKey("UserId")]public MemberDuty? Duty { get; set; }
+    public DateOnly Date { get; set; }
+}
