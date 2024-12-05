f7579930973fd516a241c306cfaf765306e90672
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-05 22:37:45 +0400

    Вынес в цикл 2

--- GeneratorDuty/BackgroundServices/AutoSendSchedule.cs
+++ GeneratorDuty/BackgroundServices/AutoSendSchedule.cs
@@ -1,5 +1,4 @@
 ﻿using System.Globalization;
-using System.Timers;
 using ClientSamgk;
 using GeneratorDuty.Common;
 using GeneratorDuty.Extensions;
@@ -10,77 +9,84 @@ using Telegram.Bot;
 
 namespace GeneratorDuty.BackgroundServices;
 
-public class AutoSendSchedule(ITelegramBotClient client, DutyRepository repository, 
-    ClientSamgkApi clientSamgkApi, ILogger<AutoSendSchedule> logger) : BackgroundServiceBase
+public class AutoSendSchedule(
+    ITelegramBotClient client,
+    DutyRepository repository,
+    ClientSamgkApi clientSamgkApi,
+    ILogger<AutoSendSchedule> logger) : BackgroundServiceBase
 {
-    private async void OnEventExecution(object? sender, ElapsedEventArgs e)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var dateTime = DateTime.Now;
-        
-        if(!CanWorkSerivce(DateTime.Now)) return;
-        
-        logger.LogInformation($"Запуск скрипта по расписанию");
-        
-        var scheduleProps = await repository.ScheduleProps.GetSchedulePropsFromAutoSend(true);
-        
-        // если время вечернее смотрим расписание на перед
-        if (dateTime.Hour >= 10) dateTime = dateTime.AddDays(1);
-            
-        // если день выходной, то пропускаем и добавляем дни пока не попадется рабочий
-        while (dateTime.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday) dateTime = dateTime.AddDays(1);
-        
-        foreach (var item in scheduleProps)
-        {
-            logger.LogInformation($"Скрипт № {item.Id} начал работать");
-            
-            var result = await clientSamgkApi.Schedule
-                .GetScheduleAsync(DateOnly.FromDateTime(dateTime), item.SearchType, item.Value);
-
-            if (result.Lessons.Count is 0)
-            {
-                logger.LogInformation($"Скрипт № {item.Id} отработан: Расписание на {dateTime.ToString(CultureInfo.InvariantCulture)} - 0 пар");
-                continue;
-            }
-
-            var md5New = result.GetMd5();
-            
-            if (item.LastResult == md5New)
-            {
-                logger.LogInformation($"Скрипт № {item.Id} отработан: Прошлый результат: {item.LastResult}. Новый {md5New}");
-                continue;
-            }
-                
-            var success = await client.TrySendMessage(item.IdPeer, result.GetStringFromRasp());
-
-            if (!success)
-            {
-                item.Fails++;
-                logger.LogInformation($"Скрипт № {item.Id} не отработан: Ошибки при отправке сообщения");
-            }
-            
-            if (success)
-            {
-                item.Fails = 0;
-                item.LastResult = md5New;
-                logger.LogInformation($"Скрипт № {item.Id} отработан: ОК");
-            }
-            
-            await repository.ScheduleProps.Update(item);
-            if (item.Fails >= 10)
-            {
-                logger.LogInformation($"Скрипт № {item.Id} удалена из за большого кол-во ошибок");
-                await repository.ScheduleProps.Remove(item);
-            }
-            logger.LogInformation($"Скрипт № {item.Id} Завершен");
-            await Task.Delay(1000);
-        }
-    }
-
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        Timer.Elapsed += OnEventExecution;
-        Timer.Start();
         logger.LogInformation($"Запущен сервис");
-        return Task.CompletedTask;
+        
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var dateTime = DateTime.Now;
+
+            if (!CanWorkSerivce(DateTime.Now))
+            {
+                await Task.Delay(1000);
+                return;
+            }
+
+            logger.LogInformation($"Запуск скрипта по расписанию");
+
+            var scheduleProps = await repository.ScheduleProps.GetSchedulePropsFromAutoSend(true);
+
+            // если время вечернее смотрим расписание на перед
+            if (dateTime.Hour >= 10) dateTime = dateTime.AddDays(1);
+
+            // если день выходной, то пропускаем и добавляем дни пока не попадется рабочий
+            while (dateTime.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday) dateTime = dateTime.AddDays(1);
+
+            foreach (var item in scheduleProps)
+            {
+                logger.LogInformation($"Скрипт № {item.Id} начал работать");
+
+                var result = await clientSamgkApi.Schedule
+                    .GetScheduleAsync(DateOnly.FromDateTime(dateTime), item.SearchType, item.Value);
+
+                if (result.Lessons.Count is 0)
+                {
+                    logger.LogInformation(
+                        $"Скрипт № {item.Id} отработан: Расписание на {dateTime.ToString(CultureInfo.InvariantCulture)} - 0 пар");
+                    continue;
+                }
+
+                var md5New = result.GetMd5();
+
+                if (item.LastResult == md5New)
+                {
+                    logger.LogInformation(
+                        $"Скрипт № {item.Id} отработан: Прошлый результат: {item.LastResult}. Новый {md5New}");
+                    continue;
+                }
+
+                var success = await client.TrySendMessage(item.IdPeer, result.GetStringFromRasp());
+
+                if (!success)
+                {
+                    item.Fails++;
+                    logger.LogInformation($"Скрипт № {item.Id} не отработан: Ошибки при отправке сообщения");
+                }
+
+                if (success)
+                {
+                    item.Fails = 0;
+                    item.LastResult = md5New;
+                    logger.LogInformation($"Скрипт № {item.Id} отработан: ОК");
+                }
+
+                await repository.ScheduleProps.Update(item);
+                /*if (item.Fails >= 10)
+                {
+                    logger.LogInformation($"Скрипт № {item.Id} удалена из за большого кол-во ошибок");
+                    await repository.ScheduleProps.Remove(item);
+                }*/
+
+                logger.LogInformation($"Скрипт № {item.Id} Завершен");
+                await Task.Delay(1000);
+            }
+        }
     }
 }
