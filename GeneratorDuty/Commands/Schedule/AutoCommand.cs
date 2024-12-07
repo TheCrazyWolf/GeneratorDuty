c487f51f893f66e698392fe865fbd45d1aa4a054
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-07 17:41:58 +0400

    Обновил команду на авто-рассылку расписания

--- GeneratorDuty/Commands/Schedule/AutoCommand.cs
+++ GeneratorDuty/Commands/Schedule/AutoCommand.cs
@@ -10,10 +10,9 @@ public class AutoCommand(DutyRepository repository) : BaseCommand
 {
     public override string Command { get; } = "/auto";
 
-    private readonly string _usage = "ℹ️ На эту беседу нет установленных данных. Используйте /set <фио препода/группа/кабинет>";
-    private readonly string _success = "✅ Настройки этой беседы обновлены";
-    private readonly string _unSuccess = "ℹ️ Не удалось обновить настройки. Usage: /auto true/false";
-    
+    private readonly string _usage =
+        "ℹ️ На эту беседу нет установленных данных. Используйте /set <фио препода/группа/кабинет>";
+
     public override async Task ExecuteAsync(ITelegramBotClient client, Message message)
     {
         if (string.IsNullOrEmpty(message.Text) || message.From is null) return;
@@ -28,15 +27,13 @@ public class AutoCommand(DutyRepository repository) : BaseCommand
             return;
         }
 
-        if (!bool.TryParse(message.Text.Split(' ')[1], out bool result))
-        {
-            await client.TrySendMessage(message.Chat.Id, _unSuccess);
-            return;
-        }
-        
-        prop.IsAutoSend = result;
+        prop.IsAutoSend = !prop.IsAutoSend;
         await repository.ScheduleProps.Update(prop);
-        
-        await client.TrySendMessage(message.Chat.Id, _success);
+
+        string messageNotify = prop.IsAutoSend
+            ? "✅ Авто-рассылка расписания включена"
+            : "ℹ️ Авто-рассылка расписания выключена";
+
+        await client.TrySendMessage(message.Chat.Id, messageNotify);
     }
 }
