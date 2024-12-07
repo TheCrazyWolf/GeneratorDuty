9203c348051f2abd5def5e8ad8f08ddbf33a8453
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-07 19:28:37 +0400

    Миграции

---
+++ GeneratorDuty/Migrations/20241207151144_v1.2.1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneratorDuty.Migrations
+{
+    /// <inheritdoc />
+    public partial class v121 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_History_ScheduleProps_IdPeer",
+                table: "History");
+
+            migrationBuilder.DropIndex(
+                name: "IX_History_IdPeer",
+                table: "History");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_History_IdPeer",
+                table: "History",
+                column: "IdPeer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_History_ScheduleProps_IdPeer",
+                table: "History",
+                column: "IdPeer",
+                principalTable: "ScheduleProps",
+                principalColumn: "Id");
+        }
+    }
+}
