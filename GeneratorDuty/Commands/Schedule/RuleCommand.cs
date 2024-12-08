c14f8ff878a94033c753cf8d50fccedc93f5fa29
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-08 09:33:48 +0400

    команда на корректировка

--- GeneratorDuty/Commands/Schedule/RuleCommand.cs
+++ GeneratorDuty/Commands/Schedule/RuleCommand.cs
@@ -1,6 +1,9 @@
+using ClientSamgkOutputResponse.Enums;
 using GeneratorDuty.Common;
 using GeneratorDuty.Extensions;
+using GeneratorDuty.Models.Schedule;
 using GeneratorDuty.Repository;
+using Microsoft.EntityFrameworkCore;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -9,16 +12,43 @@ namespace GeneratorDuty.Commands.Schedule;
 public class RuleCommand(DutyRepository repository) : BaseCommand
 {
     private readonly long _idAdmin = 208049718;
-    public override string Command { get; } = "/say";
+    public override string Command { get; } = "/rule";
 
     public override async Task ExecuteAsync(ITelegramBotClient client, Message message)
     {
         if (string.IsNullOrEmpty(message.Text) || message.From is null || message.From.Id != _idAdmin) return;
 
-        message.Text = message.Text.GetReplacedCommandFromDomain().Replace(Command, string.Empty);
+        var array = message.Text.GetReplacedCommandFromDomain().Replace(Command, string.Empty).Split(" ");
 
-        
-       // var rule = 
-        
+        if (!Enum.TryParse<ScheduleCallType>(array[2], true, out var callType) || !DateTime.TryParse(array[1], out var date) 
+            || !bool.TryParse(array[3], out var showImportant) || !bool.TryParse(array[4], out var showHorizont ))
+        {
+            
+            var values = Enum.GetValues(typeof(ScheduleCallType)).Cast<ScheduleCallType>();
+            string build = string.Join(", ", values);
+            await client.TrySendMessage(message.Chat.Id, $"/rule дата тип разо важ гориз \n\n{build}");
+            return;
+        }
+
+        var rule = await repository.ScheduleRules.GetRuleFromDate(DateOnly.FromDateTime(date));
+
+        if (rule is null)
+        {
+            await repository.ScheduleRules.CreateRule(new ScheduleCustomRules()
+            {
+                Date = DateOnly.FromDateTime(date),
+                CallType = callType,
+                ShowImportantLesson = showImportant,
+                ShowRussianHorizont = showHorizont
+            });
+        }
+        else
+        {
+            rule.CallType = callType;
+            rule.ShowImportantLesson = showImportant;
+            rule.ShowRussianHorizont = showHorizont;
+            await repository.ScheduleRules.UpdateRule(rule);
+        }
+        await client.TrySendMessage(message.Chat.Id, "Ok");
     }
 }
