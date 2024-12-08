73f61aba65e97b310b25f75210404b92d1204234
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-08 09:12:41 +0400

    Добавил миграции

--- GeneratorDuty/Migrations/20241208051042_v1.3.Designer.cs
+++ GeneratorDuty/Migrations/20241208051042_v1.3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GeneratorDuty.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeneratorDuty.Migrations
 {
     [DbContext(typeof(DutyContext))]
-    partial class DutyContextModelSnapshot : ModelSnapshot
+    [Migration("20241208051042_v1.3")]
+    partial class v13
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
---
+++ GeneratorDuty/Migrations/20241208051042_v1.3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneratorDuty.Migrations
+{
+    /// <inheritdoc />
+    public partial class v13 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ScheduleCustomRules",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    CallType = table.Column<int>(type: "INTEGER", nullable: false),
+                    ShowImportantLesson = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ShowRussianHorizont = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleCustomRules", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduleCustomRules");
+        }
+    }
+}
