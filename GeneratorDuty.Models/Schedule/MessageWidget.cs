d922052acff7b3708fa8c54b2e866923e146d47f
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-09 22:18:51 +0400

    Перенос моделей

---
+++ GeneratorDuty.Models/Schedule/MessageWidget.cs
@@ -0,0 +1,9 @@
+﻿using GeneratorDuty.Models.Common;
+
+namespace GeneratorDuty.Models.Schedule;
+
+public class MessageWidget : CommonEntity
+{
+    public long ChatId { get; set; }
+    public int MessageId { get; set; }
+}
---
+++ GeneratorDuty/Commands/Schedule/WidgetCommand.cs
@@ -0,0 +1,35 @@
+using ClientSamgk;
+using GeneratorDuty.Common;
+using GeneratorDuty.Extensions;
+using GeneratorDuty.Repository;
+using GeneratorDuty.Utils;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace GeneratorDuty.Commands.Schedule;
+
+public class WidgetCommand(DutyRepository repository, ClientSamgkApi clientSamgk) : BaseCommand
+{
+    public override string Command { get; } = "/widget";
+
+    public override async Task ExecuteAsync(ITelegramBotClient client, Message message)
+    {
+        if (string.IsNullOrEmpty(message.Text) || message.From is null) return;
+
+        message.Text = message.Text.GetReplacedCommandFromDomain().Replace(Command, string.Empty);
+        
+        var prop = await repository.ScheduleProps.GetSchedulePropFromChat(message.Chat.Id);
+        
+        if(prop is null)
+        {
+            await client.TrySendMessage(message.Chat.Id, $"ℹ️ Не смог найти настройки для Вашей беседы, задай /set <группа, фио препода, кабинет>");
+            return;
+        }
+
+        var scheduleProp = await client.TrySendMessage(prop.IdPeer, "эт сообщение будет автоматически обновлено");
+        
+        
+        
+    }
+}
