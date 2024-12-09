51483a831d9ec426b072ba7dacc29792d3157ff8
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-09 23:37:29 +0400

    Миграции

---
+++ GeneratorDuty/Migrations/20241209185057_v1.3.1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneratorDuty.Migrations
+{
+    /// <inheritdoc />
+    public partial class v131 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ChatId",
+                table: "History",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPinned",
+                table: "History",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MessageId",
+                table: "History",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "MessageWidgets",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ChatId = table.Column<long>(type: "INTEGER", nullable: false),
+                    MessageId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageWidgets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessageWidgets");
+
+            migrationBuilder.DropColumn(
+                name: "ChatId",
+                table: "History");
+
+            migrationBuilder.DropColumn(
+                name: "IsPinned",
+                table: "History");
+
+            migrationBuilder.DropColumn(
+                name: "MessageId",
+                table: "History");
+        }
+    }
+}
