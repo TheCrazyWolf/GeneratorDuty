21cd05c399f4703e5794640cdbbad3fe9a13e56b
kulagin <alex_kulagin@samgk.ru>
2024-12-10 11:02:12 +0400

    Убрал дубляж кода

--- GeneratorDuty/BackgroundTasks/AutoSendSchedule.cs
+++ GeneratorDuty/BackgroundTasks/AutoSendSchedule.cs
@@ -15,10 +15,97 @@ public class AutoSendSchedule(
     ClientSamgkApi clientSamgkApi,
     ILogger<AutoSendSchedule> logger) : BackgroundServiceBase
 {
+    public virtual int DaysCanBeChecked { get; set; } = 2;
+    public virtual bool IsMigrated { get; set; } = false;
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         logger.LogInformation($"Запущен сервис");
-        
+
+        while (true)
+        {
+            if (!CanWorkSerivce(DateTime.Now))
+            {
+                await Task.Delay(1000);
+                continue;
+            }
+
+            logger.LogInformation($"Запуск скрипта по расписанию");
+
+            var scheduleProps = await repository.ScheduleProps.GetSchedulePropsFromAutoSend(true, isMigrated: IsMigrated);
+
+            foreach (var scheduleProp in scheduleProps)
+            {
+                var date = DateOnly.FromDateTime(DateTime.Now);
+
+                logger.LogInformation($"Скрипт № {scheduleProp.Id} начал работать");
+                for (int i = 0; i < DaysCanBeChecked; i++)
+                {
+                    // если день выходной, то пропускаем и добавляем дни пока не попадется рабочий
+                    while (date.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday) date = date.AddDays(1);
+
+                    var rules = await repository.ScheduleRules.GetRuleFromDateOrDefault(date);
+
+                    logger.LogInformation(
+                        $"Скрипт № {scheduleProp.Id}. Проверка: {date.ToString(CultureInfo.InvariantCulture)}");
+                    var history =
+                        await repository.ScheduleHistory.CreateOrGetScheduleHistory(scheduleProp.IdPeer, date);
+
+                    var result = await clientSamgkApi.Schedule.GetScheduleAsync(date, scheduleProp.SearchType,
+                        scheduleProp.Value, rules.CallType, rules.ShowImportantLesson, rules.ShowRussianHorizont);
+
+                    if (result.Lessons.Count is 0)
+                    {
+                        logger.LogInformation(
+                            $"Скрипт № {scheduleProp.Id} отработан: Расписание на {date.ToString()} - 0 пар");
+                        await Task.Delay(1000);
+                        date = date.AddDays(1);
+                        continue;
+                    }
+
+                    if (history.Result == result.GetStringFromRasp())
+                    {
+                        logger.LogInformation(
+                            $"Скрипт № {scheduleProp.Id} отработан: Расписание на {date.ToString()} - не изменилось");
+                        await Task.Delay(1000);
+                        date = date.AddDays(1);
+                        continue;
+                    }
+
+                    var success = await client.TrySendMessage(scheduleProp.IdPeer, result.GetStringFromRasp());
+
+                    if (success is null)
+                    {
+                        scheduleProp.Fails++;
+                        logger.LogInformation(
+                            $"Скрипт № {scheduleProp.Id} не отработан: Расписание на {date.ToString()}. Ошибки при отправке сообщения");
+                    }
+
+                    if (success is not null)
+                    {
+                        scheduleProp.Fails = 0;
+                        history.Result = result.GetStringFromRasp();
+                        history.MessageId = success.MessageId;
+                        history.ChatId = success.Chat.Id;
+                        logger.LogInformation($"Скрипт № {scheduleProp.Id} отработан: Расписание на {date.ToString()}");
+                    }
+
+                    await repository.ScheduleProps.Update(scheduleProp);
+                    await repository.ScheduleHistory.UpdateScheduleHistory(history);
+                    date = date.AddDays(1);
+                    await Task.Delay(3000);
+                }
+
+                await Task.Delay(3000);
+            }
+
+            await Task.Delay(300000);
+        }
+    }
+
+    /*protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        logger.LogInformation($"Запущен сервис");
+
         while (true)
         {
             var dateTime = DateTime.Now;
@@ -40,7 +127,7 @@ public class AutoSendSchedule(
             while (dateTime.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday) dateTime = dateTime.AddDays(1);
 
             var rules = await repository.ScheduleRules.GetRuleFromDateOrDefault(DateOnly.FromDateTime(dateTime));
-            
+
             foreach (var item in scheduleProps)
             {
                 logger.LogInformation($"Скрипт № {item.Id} начал работать");
@@ -86,13 +173,13 @@ public class AutoSendSchedule(
                 {
                     logger.LogInformation($"Скрипт № {item.Id} удалена из за большого кол-во ошибок");
                     await repository.ScheduleProps.Remove(item);
-                }*/
+                }#1#
 
                 logger.LogInformation($"Скрипт № {item.Id} Завершен");
                 await Task.Delay(1500);
             }
-            
+
             await Task.Delay(300000);
         }
-    }
+    }*/
 }
--- GeneratorDuty/BackgroundTasks/AutoSendScheduleNew.cs
+++ GeneratorDuty/BackgroundTasks/AutoSendScheduleNew.cs
@@ -1,94 +1,17 @@
-﻿using System.Globalization;
-using ClientSamgk;
-using GeneratorDuty.Common;
-using GeneratorDuty.Extensions;
+﻿using ClientSamgk;
 using GeneratorDuty.Repository.Duty;
-using GeneratorDuty.Utils;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 
-namespace GeneratorDuty.BackgroundTasks;
+namespace GeneratorDuty.
+    BackgroundTasks;
 
 public class AutoSendScheduleNew(
-    ITelegramBotClient client, DutyRepository repository, ClientSamgkApi clientSamgkApi,
-    ILogger<AutoSendScheduleNew> logger) : BackgroundServiceBase
+    ITelegramBotClient client,
+    DutyRepository repository,
+    ClientSamgkApi clientSamgkApi,
+    ILogger<AutoSendSchedule> logger) : AutoSendSchedule(client, repository, clientSamgkApi, logger)
 {
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        logger.LogInformation($"Запущен сервис");
-        
-        while (true)
-        {
-            if (!CanWorkSerivce(DateTime.Now))
-            {
-                await Task.Delay(1000);
-                continue;
-            }
-
-            logger.LogInformation($"Запуск скрипта по расписанию");
-
-            var scheduleProps = await repository.ScheduleProps.GetSchedulePropsFromAutoSend(true, true);
-            
-            foreach (var scheduleProp in scheduleProps)
-            {
-                var date = DateOnly.FromDateTime(DateTime.Now);
-                
-                logger.LogInformation($"Скрипт № {scheduleProp.Id} начал работать");
-                for (int i = 0; i <= 5; i++)
-                {
-                    // если день выходной, то пропускаем и добавляем дни пока не попадется рабочий
-                    while (date.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday) date = date.AddDays(1);
-
-                    var rules = await repository.ScheduleRules.GetRuleFromDateOrDefault(date);
-                    
-                    logger.LogInformation($"Скрипт № {scheduleProp.Id}. Проверка: {date.ToString(CultureInfo.InvariantCulture)}");
-                    var history = await repository.ScheduleHistory.CreateOrGetScheduleHistory(scheduleProp.IdPeer, date);
-                    
-                    var result = await clientSamgkApi.Schedule.GetScheduleAsync(date, scheduleProp.SearchType, scheduleProp.Value, rules.CallType, rules.ShowImportantLesson, rules.ShowRussianHorizont);
-                    
-                    if (result.Lessons.Count is 0)
-                    {
-                        logger.LogInformation($"Скрипт № {scheduleProp.Id} отработан: Расписание на {date.ToString()} - 0 пар");
-                        await Task.Delay(1000);
-                        date = date.AddDays(1);
-                        continue; 
-                    }
-                    
-                    if (history.Result == result.GetStringFromRasp())
-                    {
-                        logger.LogInformation($"Скрипт № {scheduleProp.Id} отработан: Расписание на {date.ToString()} - не изменилось");
-                        await Task.Delay(1000);
-                        date = date.AddDays(1);
-                        continue;
-                    }
-                    
-                    var success = await client.TrySendMessage(scheduleProp.IdPeer, result.GetStringFromRasp());
-
-                    if (success is null)
-                    {
-                        scheduleProp.Fails++;
-                        logger.LogInformation($"Скрипт № {scheduleProp.Id} не отработан: Расписание на {date.ToString()}. Ошибки при отправке сообщения");
-                    }
-
-                    if (success is not null)
-                    {
-                        scheduleProp.Fails = 0;
-                        history.Result = result.GetStringFromRasp();
-                        history.MessageId = success.MessageId;
-                        history.ChatId = success.Chat.Id;
-                        logger.LogInformation($"Скрипт № {scheduleProp.Id} отработан: Расписание на {date.ToString()}");
-                    }
-
-                    await repository.ScheduleProps.Update(scheduleProp);
-                    await repository.ScheduleHistory.UpdateScheduleHistory(history);
-                    date = date.AddDays(1);
-                    await Task.Delay(3000);
-                }
-                
-                await Task.Delay(3000);
-            }
-            
-            await Task.Delay(300000);
-        }
-    }
+    public override int DaysCanBeChecked { get; set; } = 5;
+    public override bool IsMigrated { get; set; } = true;
 }
